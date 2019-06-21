12e95e11dd7278808ed8efe92c32c569f0dec2b5
leighigoe <leigh@omobono.com>
2019-06-21 02:02:05 +0100

    Updates

---
+++ precache-manifest.2cb568503e870d6c7a94806230e7d6c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a52368c182b3f0bf092b553c2068888a",
+    "url": "/igocode/static/media/logo-yellow.a52368c1.svg"
+  },
+  {
+    "revision": "8a0d256ec2e35ec64b455dc295fb6e1f",
+    "url": "/igocode/static/media/logo-black.8a0d256e.svg"
+  },
+  {
+    "revision": "0467c83d02b4dadeda74",
+    "url": "/igocode/static/js/runtime~main.36d4f598.js"
+  },
+  {
+    "revision": "51f5154249303ab2184d",
+    "url": "/igocode/static/js/main.d55505dd.chunk.js"
+  },
+  {
+    "revision": "da6c782965ea010c0c8f",
+    "url": "/igocode/static/js/2.11f058d7.chunk.js"
+  },
+  {
+    "revision": "51f5154249303ab2184d",
+    "url": "/igocode/static/css/main.8d622bbd.chunk.css"
+  },
+  {
+    "revision": "775d63a94c4431d1a2fae188bb2b69bb",
+    "url": "/igocode/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.57e2cb1570b9832a2f936882b16b8f1d.js"
+  "/igocode/precache-manifest.2cb568503e870d6c7a94806230e7d6c1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/igocode/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
